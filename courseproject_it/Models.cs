258f9145f792f0dc76428d46e15c9778a515ebbc
SofiFairyTell <fairytellsblog@gmail.com>
2020-04-07 00:03:26 +0300

    Add models for Table

--- courseproject_it/Models.cs
+++ courseproject_it/Models.cs
@@ -1,12 +1,63 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.Data.Entity;
 namespace courseproject_it
 {
     class Models
     {
+
+   public class Person //освидетельствуемый
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Middlename { get; set; }
+        public string Surname { get; set; }
+        public string Category_Person {get;set;}//категория_освидетельствуемого
+        public string Arm_Status {get;set;}//
+        public string Birthday {get;set;}//позже сделать через поле времени
+        public string Article_Trebovanie {get;set;}
+        public string Article_Raspisanie {get;set;}
+        public string Diagnos {get;set;}
+        public string Category_Godnost {get;set;}
+        public string Result {get;set;} //заключение
+        public int Result_Id {get;set;}
+        public string Result_Date {get;set;}
+        public string Result_Prichina {get;set;}
+        public string Other {get;set;}      
     }
+//таблицы для подстановки значений в таблицу Освидетельствуемый
+    public class Category_Godnost
+    {
+    	public int Id {get;set;}
+    	public string Category {get;set;}
+    	public string Description {get;set;}
+    }
+    public class Category_Person
+    {
+    	public string Category {get;set;}
+    	public string Description {get;set;}
+    }
+    public class Result_Prichina
+    {
+    	public string Prichina {get;set;}
+    	public string Description {get;set;}
+    }
+    public class Arm_Status
+    {
+    	public string Status {get;set;}
+    	public string Description {get;set;}
+    }
+    }
+class ResultMedContext : DbContext
+        {
+        public ResultMedContext()
+           : base("DBConnection")
+            { }
+
+            public DbSet<Person> Persons { get; set; }
+            //public DbSet<Team> Teams {get;set;}
+        }
+}
+
 }
