aa10b94bfa8cba48e678c3f812b329276441187a
SofiFairyTell <fairytellsblog@gmail.com>
2020-04-11 21:24:53 +0300

    Add tables, add form. First test run. Without saving.

--- courseproject_it/Form1.cs
+++ courseproject_it/Form1.cs
@@ -7,14 +7,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.Entity;
+using courseproject_it;
 namespace courseproject_it
 {
     public partial class Form1 : Form
     {
+        ResultMedContext db;
         public Form1()
         {
             InitializeComponent();
+            db = new ResultMedContext();
+            db.Persons.Load();
+            dataGridView1.DataSource = db.Persons.Local.ToBindingList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
