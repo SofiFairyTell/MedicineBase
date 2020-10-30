971767386247f3db21930bf3a95744c7400f6581
Софья Курбатова <sofiakurbatova@outlook.com>
2020-10-30 11:46:24 +0300

    start project again. create database wit models

---
+++ courseproject_it/AddForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using courseproject_it_models;
+namespace courseproject_it
+{
+    public partial class AddForm : Form
+    {
+       // ResultMedContext db;
+        public AddForm()
+        {
+            InitializeComponent();
+           // db = new ResultMedContext();
+          // db.Persons.Load();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void AddForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void armStatusBindingSource_CurrentChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ courseproject_it/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace courseproject_it.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<courseproject_it_models.ResultMedContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "courseproject_it_models.ResultMedContext";
+        }
+
+        protected override void Seed(courseproject_it_models.ResultMedContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
