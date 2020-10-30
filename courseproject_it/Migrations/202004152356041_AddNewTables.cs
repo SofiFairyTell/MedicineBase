b0c4fdf4f48fb89c85e7e1d4c17d3b9f8f49008a
Софья Курбатова <sofiakurbatova@outlook.com>
2020-10-30 11:46:24 +0300

    start project again. create database wit models

---
+++ courseproject_it/Migrations/202004152356041_AddNewTables.cs
@@ -0,0 +1,66 @@
+﻿namespace courseproject_it.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNewTables : DbMigration
+    {
+        public override void Up()
+        {
+            RenameTable(name: "dbo.Person", newName: "Освидетельствуемый");
+            CreateTable(
+                "dbo.Категория освидетельствуемого",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Category = c.String(),
+                        Description = c.String(),
+                        Person_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Освидетельствуемый", t => t.Person_Id)
+                .Index(t => t.Person_Id);
+            
+            CreateTable(
+                "dbo.Воинское звание",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Status = c.String(),
+                        Description = c.String(),
+                        Person_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Освидетельствуемый", t => t.Person_Id)
+                .Index(t => t.Person_Id);
+            
+            CreateTable(
+                "dbo.Причинно-следственные связи",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Prichina = c.String(),
+                        Description = c.String(),
+                        Person_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Освидетельствуемый", t => t.Person_Id)
+                .Index(t => t.Person_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Причинно-следственные связи", "Person_Id", "dbo.Освидетельствуемый");
+            DropForeignKey("dbo.Воинское звание", "Person_Id", "dbo.Освидетельствуемый");
+            DropForeignKey("dbo.Категория освидетельствуемого", "Person_Id", "dbo.Освидетельствуемый");
+            DropIndex("dbo.Причинно-следственные связи", new[] { "Person_Id" });
+            DropIndex("dbo.Воинское звание", new[] { "Person_Id" });
+            DropIndex("dbo.Категория освидетельствуемого", new[] { "Person_Id" });
+            DropTable("dbo.Причинно-следственные связи");
+            DropTable("dbo.Воинское звание");
+            DropTable("dbo.Категория освидетельствуемого");
+            RenameTable(name: "dbo.Освидетельствуемый", newName: "Person");
+        }
+    }
+}
